4c18854032a972e37e11251b02256c0b0384d40a
MODARA3A <sohaibibrahim14@gmail.com>
2022-09-01 10:32:38 +0200

    new commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udacityapi-env.eba-yywd9hfv.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udacityapi-env.eba-yywd9hfv.us-east-1.elasticbeanstalk.com/api/v0/feed',
 };
 
 /*
